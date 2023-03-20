fab182e7606053da7bdd7eaa42e511cd7005a151
Dingunus <102311008+Dingunus@users.noreply.github.com>
2023-03-20 15:42:22 +0000

    on a tej l'ancien main test_sim.c

--- .ccls-cache/@home@runner@Projet-A3-G1-AN-HC/test_sim.c
+++ .ccls-cache/@home@runner@Projet-A3-G1-AN-HC/test_sim.c
@@ -8,23 +8,7 @@
 
 int main(void) {
   
-  printf("Bonjour\n");
 
- temp_t temperature;
-  temperature.interieure = 21.79;
-  temperature.exterieure = 10.84;
-
-  
-  
-  visualisationT(temperature);
-
-  float cgsn = 0;
-  cgsn = consigne(cgsn);
-
-  float puissance = regulation(1, temperature.interieure, cgsn);
-  printf("%.2f", puissance);
-
-  visualisationC(puissance);
   
   
   return 0;
---
+++ .ccls-cache/@home@runner@Projet-A3-G1-AN-HC/test_simulateur.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "simulateur.h"
+#include "consigne.h"
+#include "regulation.h"
+#include "visualisationC.h"
+#include "visualisationT.h"
+
+int main(){
+
+	temp_t temperature;
+	temperature.exterieure = 14.0;
+	temperature.interieure = 15.0;
+	struct simParam_s*  monSimulateur_ps = simConstruct(temperature); // creation du simulateur, puissance intialisée à 0%
+	int i=0; // increment de boucle
+	float puissance = 70.0; // puissance de chauffage
+	for(i=0;i< 30;i++){
+		temperature=simCalc(puissance,monSimulateur_ps); // simulation de l'environnement
+	}
+	simDestruct(monSimulateur_ps); // destruction de simulateur
+
+  printf("Bonjour\n");
+  
+  
+  visualisationT(temperature);
+
+  float cgsn = 0;
+  cgsn = consigne(cgsn);
+
+  puissance = regulation(1, temperature.interieure, cgsn);
+  printf("%.2f", puissance);
+
+  visualisationC(puissance);
+
+
+  
+	return EXIT_SUCCESS;
+}
