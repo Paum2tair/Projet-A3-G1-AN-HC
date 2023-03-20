ba428863de4881f107b258c4e9ab350cc029ed58
Dingunus <102311008+Dingunus@users.noreply.github.com>
2023-03-20 13:49:38 +0000

    consigne c fait

--- .ccls-cache/@home@runner@Projet-A3-G1-AN-HC/consigne.c
+++ .ccls-cache/@home@runner@Projet-A3-G1-AN-HC/consigne.c
@@ -1,19 +1,21 @@
 #include "consigne.h"
 
    float consigne(float csgn)
-   {
-      char cons[8];
-      FILE *consigne;
-      if ((consigne = fopen("consigne.txt", "r")) == NULL){
-        printf("Impossibe d'ouvrir le fichier consigne.txt\n");
-        return 0;
-      }
-      /*while(fgets(cons, 8, consigne) != NULL){
+    {
+      char cons[8];//passke pk pa
+      if (access(".verrouConsigne", 0) != 0) {
+        FILE *consigne;
+        if ((consigne = fopen("consigne.txt", "r")) == NULL){
+          printf("Impossibe d'ouvrir le fichier consigne.txt\n");
+          return 0;
+        }
+        /*while(fgets(cons, 8, consigne) != NULL){
               printf("%s", cons);
-
-      }*/
-      fgets(cons, 8, consigne);
-      fclose(consigne);
+        }*/
+        fgets(cons, 8, consigne);
+        fclose(consigne);
+      }
       csgn = (float)*cons;
       return csgn;
-   }
+      }
+      
--- .ccls-cache/@home@runner@Projet-A3-G1-AN-HC/regulation.h
+++ .ccls-cache/@home@runner@Projet-A3-G1-AN-HC/regulation.h
@@ -5,7 +5,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Pensez à bien proteger vos .h avec des ifndef
 
 float regulationTest(int regul, float consigne, float *tabT, int nT);
 
